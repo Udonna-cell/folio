70023d199e145d381bb14ffb342fbfecfda028b2
udonna-cell <orabuezestanley@gmail.com>
2024-01-12 14:13:17 +0100

    mobile view responsive is now in action

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,12 +9,14 @@ burger?.addEventListener("click", () => {
     if (!isNavOpen) {
         body.style.overflow = "hidden";
         nav?.classList.add("open");
+        nav?.classList.remove("close")
         icon!.src = "./assets/images/Frame 60 (1).svg";
     } else {
         body.style.overflow = "scroll";
+        nav?.classList.add("close")
         nav?.classList.remove("open");
         icon!.src = "./assets/images/Frame 60.svg";
     }
 
-    isNavOpen = !isNavOpen; // Toggle the state
+    isNavOpen = !isNavOpen; // Toggle the state 
 });
